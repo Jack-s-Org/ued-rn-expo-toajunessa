3930a3658d3486d16a4d575260d86fffea84e245
toajunessa <2205205@student.toa.edu.my>
2024-08-07 11:48:39 +0800

    Change text on Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home</Text>
-      <Text>Home Screen</Text>
+      <Text>Home Screen 2</Text>
     </SafeAreaView>
   );
 };
